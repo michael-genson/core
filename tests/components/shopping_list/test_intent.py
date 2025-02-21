0d8dec685cb15274ba470a9cc6a1c69103f9ad50
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-02-21 15:33:15 +0000

    moved intent tests to proper file

--- tests/components/shopping_list/test_intent.py
+++ tests/components/shopping_list/test_intent.py
@@ -1,7 +1,42 @@
 """Test Shopping List intents."""
 
+from homeassistant.components.shopping_list.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import intent
+
+
+async def test_add_item(hass: HomeAssistant, sl_setup) -> None:
+    """Test adding an item intent."""
+
+    response = await intent.async_handle(
+        hass, "test", "HassShoppingListAddItem", {"item": {"value": " beer "}}
+    )
+    assert len(hass.data[DOMAIN].items) == 1
+    assert hass.data[DOMAIN].items[0]["name"] == "beer"  # name was trimmed
+
+    # Response text is now handled by default conversation agent
+    assert response.response_type == intent.IntentResponseType.ACTION_DONE
+
+
+async def test_complete_item(hass: HomeAssistant, sl_setup) -> None:
+    """Test completing an item intent."""
+
+    await intent.async_handle(
+        hass, "test", "HassShoppingListAddItem", {"item": {"value": "beer"}}
+    )
+    await intent.async_handle(
+        hass, "test", "HassShoppingListAddItem", {"item": {"value": "cheese"}}
+    )
+
+    response = await intent.async_handle(
+        hass, "test", "HassShoppingListCompleteItem", {"item": {"value": "beer"}}
+    )
+    assert len(hass.data[DOMAIN].items) == 2
+    assert hass.data[DOMAIN].items[0]["name"] == "beer"
+    assert hass.data[DOMAIN].items[0]["complete"] is True
+
+    # Response text is now handled by default conversation agent
+    assert response.response_type == intent.IntentResponseType.ACTION_DONE
 
 
 async def test_recent_items_intent(hass: HomeAssistant, sl_setup) -> None:
